473532670075395cc38e5c69a8983b7ca1202e23
h4rdw1r3 <vinaugust6@gmail.com>
2021-06-13 11:20:58 +0530

    update property names
    use updated schema and update property name accordingly

--- podcasts.go
+++ podcasts.go
@@ -3,25 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
 
 type podcast struct {
-	Id             int64  `json:"id"`
-	Title          string `json:"title"`
-	Description    string `json:"description"`
-	WebsiteAddress string `json:"website_address"`
-	Category_id    int64  `json:"category_id"`
-	Language       string `json:"language"`
-	IsExplicit     bool   `json:"isExplicit"`
-	Cover_art_path string `json:"cover_art_path"`
-	Author_name    string `json:"author_name"`
-	Author_email   string `json:"author_email"`
-	Copyright      string `json:"copyright"`
-	Created_at     string `json:"created_at"`
-	Updated_at     string `json:"updated_at"`
-	Published_at   string `json:"published_at"`
+	ID             int64     `json:"id"`
+	Title          string    `json:"title"`
+	Description    string    `json:"description"`
+	WebsiteAddress string    `json:"website_address"`
+	CategoryID     int64     `json:"category_id"`
+	Language       string    `json:"language"`
+	IsExplicit     bool      `json:"isExplicit"`
+	CoverArtID     int64     `json:"cover_art_id"`
+	AuthorName     string    `json:"author_name"`
+	AuthorEmail    string    `json:"author_email"`
+	Copyright      string    `json:"copyright"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
 }
 
 func createPodcast(c echo.Context) (err error) {
@@ -32,8 +32,17 @@ func createPodcast(c echo.Context) (err error) {
 		return
 	}
 
-	q := `INSERT INTO podcasts(title,description,website_address,category_id,languages,is_Explicit,cover_art_path,author_name,author_email,copyright,created_at,updated_at,published_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13) RETURNING podcast_id`
-	err = db.QueryRow(q, pd.Title, pd.Description, pd.WebsiteAddress, pd.Category_id, pd.Language, pd.IsExplicit, pd.Cover_art_path, pd.Author_name, pd.Author_email, pd.Copyright, pd.Created_at, pd.Updated_at, pd.Published_at).Scan(&pd.Id)
+	pd.CreatedAt = time.Now()
+	pd.UpdatedAt = time.Now()
+
+	q := `
+		INSERT INTO podcasts(
+			title, description, website_address, category_id, language, 
+			is_explicit, cover_art_id, author_name, author_email, copyright, 
+			created_at, updated_at
+		) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12) RETURNING id
+		`
+	err = db.QueryRow(q, pd.Title, pd.Description, pd.WebsiteAddress, pd.CategoryID, pd.Language, pd.IsExplicit, pd.CoverArtID, pd.AuthorName, pd.AuthorEmail, pd.Copyright, pd.CreatedAt, pd.UpdatedAt).Scan(&pd.ID)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -46,7 +55,9 @@ func getPodcast(c echo.Context) (err error) {
 
 	var pds []podcast
 
-	q := `SELECT podcast_id,title,description,website_address,category_id,languages,is_Explicit,cover_art_path,author_name,author_email,copyright,created_at,updated_at,published_at FROM podcasts`
+	q := `
+		SELECT id, title, description, website_address, category_id, language, is_explicit, cover_art_id, author_name, author_email,copyright, created_at, updated_at FROM podcasts
+	`
 
 	rows, err := db.Query(q)
 	if err != nil {
@@ -57,7 +68,7 @@ func getPodcast(c echo.Context) (err error) {
 	for rows.Next() {
 		var pd podcast
 
-		err = rows.Scan(&pd.Id, &pd.Title, &pd.Description, &pd.WebsiteAddress, &pd.Category_id, &pd.Language, &pd.IsExplicit, &pd.Cover_art_path, &pd.Author_name, &pd.Author_email, &pd.Copyright, &pd.Created_at, &pd.Updated_at, &pd.Published_at)
+		err = rows.Scan(&pd.ID, &pd.Title, &pd.Description, &pd.WebsiteAddress, &pd.CategoryID, &pd.Language, &pd.IsExplicit, &pd.CoverArtID, &pd.AuthorName, &pd.AuthorEmail, &pd.Copyright, &pd.CreatedAt, &pd.UpdatedAt)
 		if err != nil {
 			fmt.Println(err)
 		}
