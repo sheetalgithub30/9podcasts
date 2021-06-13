c835518741a76f832db7e010206d3add49137721
h4rdw1r3 <vinaugust6@gmail.com>
2021-06-13 11:56:24 +0530

    update episodes using new schema

--- episodes.go
+++ episodes.go
@@ -3,23 +3,27 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo"
 )
 
 type episode struct {
-	Id                   int64  `json:"id"`
-	Title                string `json:"title"`
-	Description          string `json:"description"`
-	Season               int64  `json:"season"`
-	Episode              int64  `json:"episode"`
-	Type_of_episode      string `json:"type_of_episode"`
-	IsExplicit           bool   `json:"isExplicit"`
-	Episode_cover_art    string `json:"episode_cover_art"`
-	Episode_content_path string `json:"episode_content_path"`
-	Published            bool   `json:"published"`
-	Created_at           string `json:"created_at"`
-	Updated_at           string `json:"updated_at"`
+	ID          int64  `json:"id"`
+	PodcastID   int64  `json:"podcast_id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	SeasonNo    int    `json:"season_no"`
+	EpisodeNo   int    `json:"episode_no"`
+	// 1 = Full | 2 = Bonus | 3 = trailer
+	TypeOfEpisode    int       `json:"type_of_episode"`
+	IsExplicit       bool      `json:"isExplicit"`
+	EpisodeArtID     int64     `json:"episode_art_id"`
+	EpisodeContentID int64     `json:"episode_content_id"`
+	Published        bool      `json:"published"`
+	PublishedAt      time.Time `json:"published_at"`
+	CreatedAt        time.Time `json:"created_at"`
+	UpdatedAt        time.Time `json:"updated_at"`
 }
 
 func createEpisodes(c echo.Context) (err error) {
@@ -30,8 +34,17 @@ func createEpisodes(c echo.Context) (err error) {
 		return
 	}
 
-	q := `INSERT INTO episodes (title,description,season,episode,type_of_episode,isExplicit,episode_cover_art,episode_content_path,published,created_at,updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING episode_id`
-	err = db.QueryRow(q, ep.Title, ep.Description, ep.Season, ep.Episode, ep.Type_of_episode, ep.IsExplicit, ep.Episode_cover_art, ep.Episode_content_path, ep.Published, ep.Created_at, ep.Updated_at).Scan(&ep.Id)
+	ep.CreatedAt = time.Now()
+	ep.UpdatedAt = time.Now()
+
+	q := `
+		INSERT INTO episodes (
+			title, description, season_no, episode_no, type_of_episode, 
+			is_explicit, episode_art_id, episode_content_id, 
+			published, created_at, updated_at
+		) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING id
+	`
+	err = db.QueryRow(q, ep.Title, ep.Description, ep.SeasonNo, ep.EpisodeNo, ep.TypeOfEpisode, ep.IsExplicit, ep.EpisodeArtID, ep.EpisodeContentID, ep.Published, ep.CreatedAt, ep.UpdatedAt).Scan(&ep.ID)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -44,7 +57,10 @@ func getEpisode(c echo.Context) (err error) {
 
 	var eps []episode
 
-	q := `SELECT * FROM episodes`
+	q := `SELECT id, title, description, season_no, episode_no,
+				type_of_episode, is_explicit, episode_art_id, episode_content_id,
+				published, created_at, updated_at FROM episodes
+			`
 
 	rows, err := db.Query(q)
 	if err != nil {
@@ -55,7 +71,7 @@ func getEpisode(c echo.Context) (err error) {
 	for rows.Next() {
 		var ep episode
 
-		err = rows.Scan(&ep.Id, &ep.Title, &ep.Description, &ep.Season, &ep.Episode, &ep.Type_of_episode, &ep.IsExplicit, &ep.Episode_cover_art, &ep.Episode_content_path, &ep.Published, &ep.Created_at, &ep.Updated_at)
+		err = rows.Scan(&ep.ID, &ep.Title, &ep.Description, &ep.SeasonNo, &ep.EpisodeNo, &ep.TypeOfEpisode, &ep.IsExplicit, &ep.EpisodeArtID, &ep.EpisodeContentID, &ep.Published, &ep.CreatedAt, &ep.UpdatedAt)
 		if err != nil {
 			fmt.Println(err)
 		}
